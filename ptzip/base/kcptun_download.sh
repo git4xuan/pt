9ff5a912d086a8b335de5c95699409dd4f0f519c
sc <git4xuan@gmail.com>
2017-12-17 10:13:19 +0000

    kcp

--- ptzip/base/kcptun_download.sh
+++ ptzip/base/kcptun_download.sh
@@ -1,1 +1,3 @@
-wget https://github.com/xtaci/kcptun/releases/download/v20171201/kcptun-linux-amd64-20171201.tar.gz 
+wget https://github.com/xtaci/kcptun/releases/download/v20171201/kcptun-linux-amd64-20171201.tar.gz
+tar -xzvf kcptun-linux-amd64-20171201.tar.gz && mv server_linux_amd64 /usr/bin/ && mv client_linux_amd64 /usr/bin/
+rm -f kcptun-linux-amd64-20171201.tar.gz
