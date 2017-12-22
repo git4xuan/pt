7fe7062f303c2c505adfc9cc0d40c11ac57826bf
sc <git4xuan@gmail.com>
2017-12-22 09:10:16 +0000

    ssh

--- ptzip/ssh-newkey.sh
+++ ptzip/ssh-newkey.sh
@@ -8,7 +8,7 @@
 
 #这是public的位置，最好是http的URL便于下载
 #location_pub="https://raw.githubusercontent.com/git4xuan/init/master/files/newauth.pub"
-#location_sshd="https://raw.githubusercontent.com/git4xuan/init/master/files/sshd_config"
+location_sshd="https://raw.githubusercontent.com/git4xuan/init/master/files/sshd_config"
 
 if [[ ! -d /root/.ssh/ ]]; then
     mkdir /root/.ssh
@@ -26,8 +26,8 @@ chmod 700   ~/.ssh/
 
 #更改sshd的config文件，可以使用sed，
 #但是这里为了方便改为直接wget
-#wget  --no-check-certificate -O sshd_config $location_sshd
-mv conf/ssh/sshd_config /etc/ssh/sshd_config
+wget  --no-check-certificate -O sshd_config $location_sshd
+cp sshd_config /etc/ssh/sshd_config
 
 #配置文件生效
 systemctl restart sshd.service
