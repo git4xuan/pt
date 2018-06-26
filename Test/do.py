3c57cef70a9dfe8d8d08fc901941be9730d0443a
mx01 <git4xuan@gmail.com>
2018-06-26 13:18:08 +0800

    what????
    Signed-off-by: mx01 <git4xuan@gmail.com>

--- Test/do.py
+++ Test/do.py
@@ -16,22 +16,47 @@ class Linodes(self,domain=None):
 
 
     def getOldNodeId(self):
-        pass
+        s, t = subprocess.getstatusoutput("linode-cli linodes list --json --pretty")
+        source_id = -1
+        if (s1 == 0):
+            tDict = {}
+            tList = []
+            dt1 = json.loads(t1)
+            if (type(dt1) == type(tDict)):
+                source_id = dt1["id"]
+            elif (type(dt1) == type(tList)):
+                source_id = dt1[0]["id"]
+                ## 有bug，需要ip校验，先不管
+            else:
+                print("There is problem in linode-cli output")
+        else:
+            print("list error....")
+        return source_id
 
+    def createNewNodes(self): ## By clone
+        if(self.source_id == -1):
+            return -1
+        else:
+            s, t = subprocess.getstatusoutput("linode-cli linodes clone " + str(self.source_id) + " --json --pretty")
+            return 0
+    def bootNewNodes(self):
+        if (self.target_id == -1):
+            return -1
+        else:
+            s, t = subprocess.getstatusoutput("linode-cli linodes boot " + str(self.target_id) + " --json --pretty")
+            return s
 
-
-    def getNewNodeIP(self):
-        pass
 
     def getNewNodeId(self):
+        pass
+
+    def getNewNodeIP(self):
         pass
 
     def deleteOldNodes(self):
         pass
 
-    def createNewNodes(self):
-        ##By clone
-        pass
+
 
     def getDomainIP(self):
         pass
@@ -45,6 +70,9 @@ class Linodes(self,domain=None):
     def ssClientRestart(self):
         pass
 
+    def updateSSRServer(self):
+        # exec all actions...
+        pass
 
 if __name__ == "__main__":
     s1,t1 = subprocess.getstatusoutput("linode-cli linodes list")
