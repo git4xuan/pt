f8ead0d60076085c7f53fca344406aeb25e96ff8
mingxuan <git4xuan@users.noreply.github.com>
2018-08-01 16:38:19 +0800

    change source。

--- ptzip/base/fdt_install.sh
+++ ptzip/base/fdt_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #get fdt.jar
-wget http://oy2v5m0ma.bkt.clouddn.com/fdt.jar -O /usr/local/fdt.jar
+wget https://github.com/fast-data-transfer/fdt/releases/download/0.25.1/fdt.jar -O /usr/local/fdt.jar
